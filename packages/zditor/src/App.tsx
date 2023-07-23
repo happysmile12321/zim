74c7d331bc434708f94b5ee7d5a108994dfd1bda
happysmile <812024380@qq.com>
2023-07-24 06:14:50 +0800

    想加入装饰器模式

--- packages/zditor/src/App.tsx
+++ packages/zditor/src/App.tsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const App: React.FC = () => {
-  return <>zditor</>;
+  const ref = useRef(null);
+  useEffect(() => {
+    const keyHandler = (e: KeyboardEvent) => {
+      const key = e.key;
+      // switch (key) {
+      //   case
+      // }
+    };
+
+    (document as Document).addEventListener("keypress", keyHandler);
+    return () =>
+      (document as Document).removeEventListener("keydown", keyHandler);
+  }, []);
+  return (
+    <div
+      ref={ref}
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      zditor
+    </div>
+  );
 };
 export default App;
